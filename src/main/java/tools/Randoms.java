1921e36988ee509bb7c299444687f65caa416bab
Alexey Porfirev <porfirev.alexey@gmail.com>
2022-01-26 18:13:58 +0400

    first commit

---
+++ src/main/java/tools/Randoms.java
@@ -0,0 +1,36 @@
+package tools;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+    private int min;
+    private int max;
+
+    public Randoms(int min, int max) {
+        this.min = min;
+        this.max= max;
+        this.random = new Random(System.currentTimeMillis());
+
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            @Override
+            /* Всегда возвращаем true, т.к. итератор бесконечный
+             *
+             */
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return min + random.nextInt(max - min + 1);
+            }
+        };
+    }
+
+}
